80d196d8d8396a22f08a045a4a388843f1614c1e
Leighton Albrecht <leightonalbrecht@gmail.com>
2023-01-05 15:06:45 -0500

    built structure for the portfolio page; added placeholder cards for projects

--- portfolio23/src/components/Portfolio.tsx
+++ portfolio23/src/components/Portfolio.tsx
@@ -1,11 +1,219 @@
 import { Fade } from '@mui/material'
+import { Row, Col, Card, CardTitle, CardBody, CardText, ListGroup, ListGroupItem, Button, CardFooter, CardLink } from 'reactstrap'
 import '../css/Portfolio.css'
 
 export default function Portfolio() {
     return (
         <Fade in={true}>
-            <div>
-                <h1>This is my Portfolio</h1>
+            <div className="portfolioPage">
+                <h1>Java/React Web-Apps</h1>
+                <h2>A selection of web applications I have developed or supported in developing</h2>
+                <Row>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <h1 className="projectSection2">Bootcamp projects</h1>
+                <h2>A selection of early projects to demonstrate my progress as a developer</h2>
+                <Row>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                            <CardFooter className="projectCardFooter">
+                                <p>This project has been updated, you can see the updated version <CardLink href="#">here</CardLink></p>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                            <CardFooter className="projectCardFooter">
+                                <p>This project has been updated, you can see the updated version <CardLink href="#">here</CardLink></p>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                            <CardFooter className="projectCardFooter">
+                                <p>This project has been updated, you can see the updated version <CardLink href="#">here</CardLink></p>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="6" sm="12">
+                        <Card className="projectCard">
+                            <img src="https://via.placeholder.com/480x270" alt="Placeholder" />
+                            <CardBody className="projectCardBody">
+                                <CardTitle tag="h5">
+                                    Project Title
+                                </CardTitle>
+                                <CardText>
+                                    A description of the project
+                                </CardText>
+                            </CardBody>
+                            <ListGroup className="projectCardList">
+                                <ListGroupItem className="projectCardListItem">
+                                    Technologies
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody className="projectCardBody">
+                                <Button>
+                                    Check it out
+                                </Button>
+                            </CardBody>
+                            <CardFooter className="projectCardFooter">
+                                <p>This project has been updated, you can see the updated version <CardLink href="#">here</CardLink></p>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         </Fade>
     )
